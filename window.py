a96df469c695e325fa896fe7dd1b08c3f9820143
mateusz <kontakt@mateuszmidor.com>
2014-07-07 23:54:27 +0200

    Added main menu, label and button

--- window.py
+++ window.py
@@ -1,7 +1,48 @@
 from Tkinter import *
+import tkFileDialog
 
-app = Tk()
-app.title("Pierwsze okienko w pytonie")
+class WindowDemo:
+    def run(self):
+        win = self.prepareGui()
+        win.mainloop()
 
-app.mainloop()
+    def closewindow(self):
+        exit()
+
+    def askForFile(self):
+        self.labelText.set(tkFileDialog.askopenfilename(filetypes = ( ("pyhons", "*.py"), ("any", "*.*") )))
+    
+    def prepareGui(self):
+        """prepares window and stuff"""
+        
+        app = Tk()
+        app.title("Pierwsze okienko w pytonie")
+
+        self.labelText = StringVar()
+        label = Label(app, textvariable = self.labelText)
+        label.pack()
+
+        button = Button(app, text="Otworz plik", command = self.askForFile)
+        button.pack()
+
+        
+        menubar = Menu(app)
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Otworz plik", command = self.askForFile)
+        filemenu.add_command(label="Zamknij", command= self.closewindow)
+        
+        menubar.add_cascade(label="Plik", menu=filemenu)
+
+        
+
+        app.config(menu=menubar)
+        
+      
+        return app
+
+
+
+WindowDemo().run()
+
+
     
