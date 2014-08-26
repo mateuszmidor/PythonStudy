471f645615e84fabfdc481c823dc1526a71a586d
mateusz <kontakt@mateuszmidor.com>
2014-08-26 19:12:32 +0200

    Added run command functionality to Main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from onlineview import OnlineView
+from commandexecutor import CommandExecutor
 
 class Main():
     
     @staticmethod
     def run(args):
-        output = args.getvalue("output", u"> ")
+        output = args.getvalue("output", u"")
         cmd = args.getvalue("command", u"")
-        OnlineView.render(cmd)
+        out = Main.runCommand(cmd)
+        output += cmd + u"\n" + out + u"\n"
+        OnlineView.render(output)
+        
+    @staticmethod
+    def runCommand(cmd):
+        if (cmd):
+            try:
+                out = CommandExecutor.execute(cmd)
+                return out
+            except Exception, e:
+                return unicode(e) 
+        else:
+            return u""
