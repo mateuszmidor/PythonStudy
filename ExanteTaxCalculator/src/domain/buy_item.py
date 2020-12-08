871402c1b9a314d3d193f400c6837c09a40582f0
Mateusz Midor <3demaniac@gmail.com>
2020-12-08 19:10:23 +0100

    BuyItem and SellItem hold transaction_id

--- ExanteTaxCalculator/src/domain/buy_item.py
+++ ExanteTaxCalculator/src/domain/buy_item.py
@@ -3,10 +3,11 @@ import datetime
 from money import Money
 
 class BuyItem:
-   def __init__(self, asset_name : str, amount : int, paid : Money, commision : Money, date : datetime.date):
+   def __init__(self, asset_name : str, amount : int, paid : Money, commission : Money, date : datetime.date, transaction_id: int):
       self.asset_name = asset_name
       self.amount = amount
       self.paid = paid
-      self.commision = commision
+      self.commission = commission
       self.date = date
       self.asset_left = amount # if some amount was sold
+      self.transaction_id = transaction_id
--- ExanteTaxCalculator/src/domain/sell_item.py
+++ ExanteTaxCalculator/src/domain/sell_item.py
@@ -2,9 +2,10 @@ import datetime
 from money import Money
 
 class SellItem:
-   def __init__(self, asset_name : str, amount : int, paid : Money, commision : Money, date : datetime.date):
+   def __init__(self, asset_name : str, amount : int, received : Money, commission : Money, date : datetime.date, transaction_id: int):
       self.asset_name = asset_name
       self.amount = amount
-      self.paid = paid
-      self.commision = commision
+      self.received = received
+      self.commission = commission
       self.date = date
+      self.transaction_id = transaction_id
