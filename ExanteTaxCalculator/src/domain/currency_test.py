0f4f6a201c7f8b604187c64009d4748d83f2c5f5
Mateusz Midor <3demaniac@gmail.com>
2020-12-19 08:31:16 +0100

    Added Currency.is_currency(..)

--- ExanteTaxCalculator/src/domain/currency_test.py
+++ ExanteTaxCalculator/src/domain/currency_test.py
@@ -2,6 +2,7 @@ import unittest
 from src.domain.currency import Currency
 from src.utils.capture_exception import capture_exception
 
+
 class CurrencyTest(unittest.TestCase):
     def test_cant_create_currency_2_characters(self):
         # when
@@ -30,3 +31,25 @@ class CurrencyTest(unittest.TestCase):
 
         # then
         self.assertIsInstance(e, ValueError)
+
+    def test_recognize_popular_currencies(self):
+        # given
+        currencies = ["EUR", "USD", "GBP", "CHF", "CAD", "AUD", "HKD", "JPY", "sgd", "nok", "cny"]
+
+        for cur in currencies:
+            # when
+            is_currency = Currency.is_currency(cur)
+
+            # then
+            self.assertTrue(is_currency)
+
+    def test_recognize_non_currencies(self):
+        # given
+        non_currencies = ["EU1", "USA", "GBC", "CHFa", "ECAD", "AOD"]
+
+        for cur in non_currencies:
+            # when
+            is_currency = Currency.is_currency(cur)
+
+            # then
+            self.assertFalse(is_currency)
