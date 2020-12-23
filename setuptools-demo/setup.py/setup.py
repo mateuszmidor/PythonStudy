5ee9861f1a03b8151c7f6fef77af74a29fefc041
Mateusz Midor <3demaniac@gmail.com>
2020-12-23 12:38:05 +0100

    Formatted setuptools-demo

--- setuptools-demo/setup.py/setup.py
+++ setuptools-demo/setup.py/setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup, find_packages
 
-with open('README.md') as readme:
+with open("README.md") as readme:
     long_decription = readme.read()
 
 setup(
-    name='setuptools-demo', # package will be imported as: import setuptools-demo
-    version='0.0.1',
-    description='setuptools demo',
+    name="setuptools-demo",  # package will be imported as: import setuptools-demo
+    version="0.0.1",
+    description="setuptools demo",
     long_description=long_decription,
-    author='mateuszmidor',
-    author_email='mateuszmidor@mateuszmidor.com',
-    license='MIT',
-    url='https://github.com/mateuszmidor/PythonStudy/setuptools-demo',
-    packages=find_packages(exclude=('tests*', 'testing')), # or just packages=['src']
-    install_requires=["requests>=2"], # package dependencies list
-    python_requires="~=3.5", # python 3.5 and up, but not python 4. (See: >=3.5 that includes python 4)
-    entry_points= { # this makes the installed package a shell command: setuptools-demo
-        'console_scripts': ['setuptools-demo = src.main:main']
-    }
+    long_description_content_type="text/markdown",
+    author="mateuszmidor",
+    author_email="mateuszmidor@mateuszmidor.com",
+    license="MIT",
+    url="https://github.com/mateuszmidor/PythonStudy/tree/master/setuptools-demo",
+    packages=find_packages(exclude=("tests*", "testing")),  # or just packages=['src']
+    install_requires=["requests>=2"],  # package dependencies list
+    python_requires="~=3.5",  # python 3.5 and up, but not python 4. (See: >=3.5 that includes python 4)
+    entry_points={  # this makes the installed package a shell command: setuptools-demo
+        "console_scripts": ["setuptools-demo = src.main:main"]
+    },
 )
