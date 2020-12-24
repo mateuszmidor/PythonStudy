e0b1a604c805c0b85941522f304121ef5c82fc25
Mateusz Midor <3demaniac@gmail.com>
2020-12-24 09:36:21 +0100

    Updated BuyItem SellItem to have readonly properties

--- ExanteTaxCalculator/src/domain/buy_item.py
+++ ExanteTaxCalculator/src/domain/buy_item.py
@@ -6,10 +6,34 @@ from decimal import Decimal
 
 class BuyItem:
     def __init__(self, asset_name: str, amount: Decimal, paid: Money, commission: Money, date: datetime.date, transaction_id: int):
-        self.asset_name = asset_name
-        self.amount = amount
-        self.paid = paid
-        self.commission = commission
-        self.date = date
-        self.asset_left = amount  # if some amount was sold
-        self.transaction_id = transaction_id
+        # read-only
+        self._asset_name = asset_name
+        self._amount = amount
+        self._paid = paid
+        self._commission = commission
+        self._date = date
+        self._transaction_id = transaction_id
+
+    @property
+    def asset_name(self) -> str:
+        return self._asset_name
+
+    @property
+    def amount(self) -> Decimal:
+        return self._amount
+
+    @property
+    def paid(self) -> Money:
+        return self._paid
+
+    @property
+    def commission(self) -> Money:
+        return self._commission
+
+    @property
+    def date(self) -> datetime.date:
+        return self._date
+
+    @property
+    def transaction_id(self) -> int:
+        return self._transaction_id
--- ExanteTaxCalculator/src/domain/sell_item.py
+++ ExanteTaxCalculator/src/domain/sell_item.py
@@ -5,9 +5,34 @@ from decimal import Decimal
 
 class SellItem:
     def __init__(self, asset_name: str, amount: Decimal, received: Money, commission: Money, date: datetime.date, transaction_id: int):
-        self.asset_name = asset_name
-        self.amount = amount
-        self.received = received
-        self.commission = commission
-        self.date = date
-        self.transaction_id = transaction_id
+        # read-only
+        self._asset_name = asset_name
+        self._amount = amount
+        self._received = received
+        self._commission = commission
+        self._date = date
+        self._transaction_id = transaction_id
+
+    @property
+    def asset_name(self) -> str:
+        return self._asset_name
+
+    @property
+    def amount(self) -> Decimal:
+        return self._amount
+
+    @property
+    def received(self) -> Money:
+        return self._received
+
+    @property
+    def commission(self) -> Money:
+        return self._commission
+
+    @property
+    def date(self) -> datetime.date:
+        return self._date
+
+    @property
+    def transaction_id(self) -> int:
+        return self._transaction_id
